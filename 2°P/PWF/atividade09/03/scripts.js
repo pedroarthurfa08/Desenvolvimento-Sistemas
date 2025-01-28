ce6c022654ae658f9b90436228dbbb9ffd5798cc
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-01-28 18:31:31 -0300

    comit

--- 2°P/PWF/atividade09/03/scripts.js
+++
@@ -1,9 +0,0 @@
-function getApi() {
-    const ul = document.querySelector('ul')
-    ul.innerHTML = ''
-
-    const user = document.quereySelector('#user').value
-
-    fetch(`https://api.github.com/users/${user}/repos`)
-}
-
