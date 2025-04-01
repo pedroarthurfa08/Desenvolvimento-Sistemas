04323c57e2c92aa7e68bc1b23489d26319f1335e
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-01 15:00:23 -0300

    comit

--- 3º Modulo/PWBE/lista_02/q04.js
+++ 3º Modulo/PWBE/lista_02/q04.js
@@ -24,23 +24,31 @@ class ContaBancaria {
             console.log('O valor do saque deve ser positivo.');
         }
     }
-    
-
 
     exibirSaldo() {
         console.log(`Saldo atual da conta ${this.numeroConta}: R$${this.saldo.toFixed(2)}`);
     }
+
+    transferir(contaDestino, valor) {
+        if (valor > 0 && valor <= this.saldo) {
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            console.log(`Transferência de R$${valor.toFixed(2)} realizada para a conta ${contaDestino.numeroConta}.`);
+            console.log(`Novo saldo da conta ${this.numeroConta}: R$${this.saldo.toFixed(2)}`);
+            console.log(`Novo saldo da conta ${contaDestino.numeroConta}: R$${contaDestino.saldo.toFixed(2)}`);
+        } else if (valor > this.saldo) {
+            console.log('Saldo insuficiente para a transferência.');
+        } else {
+            console.log('O valor da transferência deve ser positivo.');
+        }
+    }
 }
 
-
-
-
-
-
-
 const conta1 = new ContaBancaria(1234, 'Roberto Carlos', 500);
+const conta2 = new ContaBancaria(5678, 'Maria Silva', 300);
 
 conta1.exibirSaldo();
-conta1.depositar(200);
-conta1.sacar(100);
+conta2.exibirSaldo();
+conta1.transferir(conta2, 150);
 conta1.exibirSaldo();
+conta2.exibirSaldo();
---
+++ 3º Modulo/PWBE/lista_02/q06.js
@@ -0,0 +1,39 @@
+class Autor {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    nomeCompleto() {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+}
+
+class Livro {
+    constructor(titulo, autores, pagina) {
+        this.titulo = titulo;
+        this.autores = autores; 
+        this.pagina = pagina;
+    }
+
+    exibirAutores() {
+        console.log("Autores:");
+        this.autores.forEach(autor => {
+            console.log(autor.nomeCompleto());
+        });
+    }
+
+    exibirInfo() {
+        console.log(`Título: ${this.titulo}`);
+        this.exibirAutores(); 
+        console.log(`Número de páginas: ${this.pagina}`);
+    }
+}
+
+// Exemplo de uso
+const autor1 = new Autor("J.K.", "Rowling");
+const autor2 = new Autor("John", "Tolkien");
+
+const livro1 = new Livro("Harry Potter e a Pedra Filosofal", [autor1, autor2], 223);
+
+livro1.exibirInfo();
