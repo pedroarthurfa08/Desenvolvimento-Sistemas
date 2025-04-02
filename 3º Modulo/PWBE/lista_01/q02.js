07f886199953a266a7807deec8ee26c1b64322a4
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-02 12:52:22 -0300

    comit

---
+++ 3º Modulo/PWBE/lista_01/q02.js
@@ -0,0 +1,12 @@
+def encontrar_maior_menor():
+    num1 =int(input("Digite o primerio número:"))
+    num2 =int(input("Digite o segundo número:"))
+    num3 =int(input("Digite o terceiro número:"))
+
+    maior = max(num1, num2, num3)
+    menor = min(num1, num2, num3)
+
+    print(f"O maior número é: {maior}")
+    print(f"O menor número é: {menor}")
+
+encontrar_maior_menor()
