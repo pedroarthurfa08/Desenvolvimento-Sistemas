53937f48ea7636b4e8f65441974e09d034e7377e
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-06 19:10:25 -0300

    comti

--- 3º Modulo/DOT/lista_02/q01.py
+++ 3º Modulo/DOT/lista_02/q01.py
@@ -3,3 +3,22 @@
 # b) Grave uma lista somente com os números pares e mostre a lista;
 # c) Mostre a quantidade de números ímpares;
 # d) Grave uma lista somente com os números ímpares e mostre a lista.
+
+numeros = list(range(100))
+
+pares = []
+impares= []
+
+for n in numeros:
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+
+print("Quantidade de número pares", len(pares))
+print("Os números pares são", pares)
+
+print("--------------------------------------------------")
+
+print("Quantidade de número impares", len(impares))
+print("Os números impares são", impares)
--- 3º Modulo/DOT/lista_02/q03.py
+++ 3º Modulo/DOT/lista_02/q03.py
@@ -1,1 +1,18 @@
 # 3) Faça um programa que dada uma seqüência de n números, imprimi-la na ordem inversa à daleitura.
+
+import random
+
+n = int(input("\nQual a quantidade de números?"))
+
+numeros = []
+
+for _ in range(n):
+    num = float(input("Digite um número: "))
+    numeros.append(num)
+
+#print(numeros[::-1])
+
+#      or
+
+for num in reversed(numeros):
+    print(num)
