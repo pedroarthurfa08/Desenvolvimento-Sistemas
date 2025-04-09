42369889adf0711a5d4609595dedaae64f0cac06
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-09 19:57:59 +0000

    q14

--- 3º Modulo/DOT/lista_02/q14.py
+++ 3º Modulo/DOT/lista_02/q14.py
@@ -1,1 +1,13 @@
 # 14) Ler uma lista C de 10 elementos inteiros, trocar todos os valores negativos da lista C por 0. Escrever a lista C modificada.
+
+def modificar_lista(lista):
+    for i in range(len(lista)):
+        if lista [i] < 0:
+            lista [i] = 0
+    return lista
+
+C = [int(input(f'Elemento {i + 1}: ')) for i in range(10)]
+
+C_modificada = modificar_lista(C)
+
+print("Lista C modificada", C_modificada)
