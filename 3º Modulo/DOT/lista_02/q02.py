1288878e4cc2609e0fcf22f426e28c6ec77c6e13
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-15 18:27:19 +0000

    q02

--- 3º Modulo/DOT/lista_02/q02.py
+++ 3º Modulo/DOT/lista_02/q02.py
@@ -1,15 +1,14 @@
 # 2) Faça um programa que grave uma lista com dez números reais, calcule e mostre a quantidade de números negativos e a soma dos números positivos dessa lista.
 
-n = [1, 2, 3, 4, 5, -1, -2, -3, -4, -5, 1.5, -2.3, 4.0, -1.1, 3.3, 0.0, -4.5, 2.2, -3.3, 5.5]
+def analisa_numeros(numeros):
+    qnt_negativos = 0
+    soma_positivos = 0
 
-qnt_negativos = 0
-soma_positivos = 0
+    for numero in numeros:
+        if numero < 0:
+            qnt_negativos += 1 
+        elif numero > 0:
+            soma_positivos += 1
 
-for numero in n:
-    if numero < 0:
-        qnt_negativos += 1 
-    elif numero > 0:
-        soma_positivos += 1
-
-print("A quantidade de números negativos é", qnt_negativos)
-print("A noma dos números positivo é", soma_positivos)
+    print("A quantidade de números negativos é", qnt_negativos)
+    print("A soma dos números positivos é", soma_positivos)
