31fd28141c8e3626aaeadbf16450b5aae5de6212
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-15 18:34:47 +0000

    q07

--- 3º Modulo/DOT/lista_02/q07.py
+++ 3º Modulo/DOT/lista_02/q07.py
@@ -1,10 +1,13 @@
 # 7) Dada uma lista contendo 10 elementos numéricos, elabore um programa que verifique se um outro valor dado pertence ou não à lista.
 
+def verificar_valor_na_lista(lista, valor):
+    if valor in lista:
+        print(f"O valor {valor} está na lista.")
+    else:
+        print(f"O valor {valor} não está na lista.")
+
 lista = [10, 25, 30, 45, 50, 60, 70, 85, 90, 100]
 
 valor = float(input("Digite um valor para verificar se ele pertence à lista: "))
 
-if valor in lista:
-    print(f"O valor {valor} está na lista.")
-else:
-    print(f"O valor {valor} não está na lista.")
+verificar_valor_na_lista(lista, valor)
