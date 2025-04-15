f68c655d0e68a218360a008ed38ac3fe9b66f583
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-15 18:29:28 +0000

    q03

--- 3º Modulo/DOT/lista_02/q03.py
+++ 3º Modulo/DOT/lista_02/q03.py
@@ -1,18 +1,17 @@
 # 3) Faça um programa que dada uma seqüência de n números, imprimi-la na ordem inversa à daleitura.
 
-import random
+def ler_e_imprimir_invertido():
+    import random  
 
-n = int(input("\nQual a quantidade de números?"))
+    n = int(input("\nQual a quantidade de números? "))
+    numeros = []
 
-numeros = []
+    for _ in range(n):
+        num = float(input("Digite um número: "))
+        numeros.append(num)
 
-for _ in range(n):
-    num = float(input("Digite um número: "))
-    numeros.append(num)
+    print("\nNúmeros em ordem inversa:")
+    for num in reversed(numeros):
+        print(num)
 
-#print(numeros[::-1])
-
-    #or
-
-for num in reversed(numeros):
-    print(num)
+ler_e_imprimir_invertido()
