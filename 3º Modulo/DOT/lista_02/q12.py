c10ee902fabf9606d2b06870a6c71bd73d1e42c9
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-15 18:37:26 +0000

    q9, q12, q13

--- 3º Modulo/DOT/lista_02/q12.py
+++ 3º Modulo/DOT/lista_02/q12.py
@@ -3,20 +3,24 @@
 # o número de alunos da turma;
 # o cartão de respostas para cada aluno, contendo o seu número e suas respostas. 
 
+def corrigir_prova(num_alunos, gabarito, respostas):
+    for aluno, resposta_aluno in respostas.items():
+        acertos = 0
+        for i in range(len(gabarito)):
+            if resposta_aluno[i] == gabarito[i]:
+                acertos += 1
+        print(f'Aluno {aluno} acertou {acertos} questões.')
+
 num_alunos = 3
 
-gabarito = ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E']
+gabarito = ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E',
+            'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E',
+            'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E']
 
 respostas = {
-    101: ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E'],
-    102: ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E'],
-    103: ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E'],
+    101: ['A', 'B', 'C', 'D', 'E'] * 6,
+    102: ['A', 'B', 'C', 'D', 'E'] * 6,
+    103: ['A', 'B', 'C', 'D', 'E'] * 6,
 }
 
-for aluno, resposta_aluno in respostas.items():
-    acertos = 0
-    for i in range(30):
-        if resposta_aluno[i] == gabarito[i]:
-            acertos += 1
-
-print(f'Aluno {aluno} acertou {acertos} questões.')
+corrigir_prova(num_alunos, gabarito, respostas)
