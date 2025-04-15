68ecec65c2351e779127d452951449114c88d72e
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-15 18:31:14 +0000

    q04

--- 3º Modulo/DOT/lista_02/q04.py
+++ 3º Modulo/DOT/lista_02/q04.py
@@ -2,17 +2,20 @@
 # a) O maior elemento da lista e em que posição esse elemento se encontra;
 # b) O menor elemento da lista e em que posição esse elemento se encontra.
 
-lista = []
+def encontrar_maior_menor():
+    lista = []
 
-for i in range(15):
-    valor = int(input(f"Digite o {i + 1}º valor: "))
-    lista.append(valor)
+    for i in range(15):
+        valor = int(input(f"Digite o {i + 1}º valor: "))
+        lista.append(valor)
 
-maior = max(lista)
-menor = min(lista)
+    maior = max(lista)
+    menor = min(lista)
 
-posicao_maior = lista.index(maior)
-posicao_menor = lista.index(menor)
+    posicao_maior = lista.index(maior)
+    posicao_menor = lista.index(menor)
 
-print(f"\nO maior elemento é {maior} e está na posição {posicao_maior}.")
-print(f"\nO menor elemento é {menor} e está na posição {posicao_menor}.")
+    print(f"\nO maior elemento é {maior} e está na posição {posicao_maior}.")
+    print(f"O menor elemento é {menor} e está na posição {posicao_menor}.")
+
+encontrar_maior_menor()
