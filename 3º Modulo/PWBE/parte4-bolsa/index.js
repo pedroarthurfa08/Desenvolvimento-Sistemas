818e4c4806c1a946747b50862ecdfc7aed753248
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-23 20:40:08 -0300

    bolsa

---
+++ 3º Modulo/PWBE/parte4-bolsa/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const router = require('./router')
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static('public'))
+
+app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
---
+++ 3º Modulo/PWBE/parte4-bolsa/router.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+
+const listFaturamento = []
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+router.get('/bolsa_form', (req, res) =>{
+  res.render('bolsa_form')
+})
+
+router.post('/bolsa_resposta', (req, res) => {
+  const codigo = req.body.codigo
+  const data = req.body.data
+  const quantidade = parseInt(req.body.quantidade)
+  const preco = parseFloat(req.body.preco)
+  const tipo = req.body.tipo
+
+  const valorBruto = quantidade * preco
+  let valorLiquido
+
+  if (tipo.toLowerCase() === 'compra') {
+    valorLiquido = valorBruto + (valorBruto * 0.005)
+  } else if (tipo.toLowerCase() === 'venda') {
+    valorLiquido = valorBruto - (valorBruto * 0.005)
+  }
+
+  const elementos = [codigo, data, tipo, quantidade, preco, valorBruto, valorLiquido]
+
+  listFaturamento.push(elementos)
+
+  res.render('bolsa_resposta', {
+    listFaturamento: listFaturamento
+  })
+})
+
+module.exports = router
