698386959f2408d103f8300f40d8aa00d10d764c
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-29 14:14:34 -0300

    q02

---
+++ 3º Modulo/DOT/lista_01/q02/q02.py
@@ -0,0 +1,9 @@
+# 2. Escreva um programa que leia o raio de um círculo e faça duas funções: uma função chamada área que calcula e retorna a área do círculo e outra função chamada perímetro que calcula e retorna o perímetro do círculo. Área = PI * r2; Perímetro = PI * 2 * r;
+
+def area(r):
+    return 3.14 * r ** 2
+
+def perimetro(r):
+    p = 3.14 * 2 * r
+    return p
+
