a78083eee8738d12861720317e31b92bf9417516
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-04-29 15:22:56 -0300

    list01 q03

--- 3º Modulo/DOT/lista_01/q03/q03.py
+++ 3º Modulo/DOT/lista_01/q03/q03.py
@@ -1,4 +1,4 @@
-#3. Escreva um programa para ler uma temperatura em graus Fahrenheit. Faça uma função chamada celsius para calcular e retornar o valor correspondente em graus Celsius. Fórmula: C = ((F-32)/9)*5
+# 3. Escreva um programa para ler uma temperatura em graus Fahrenheit. Faça uma função chamada celsius para calcular e retornar o valor correspondente em graus Celsius. Fórmula: C = ((F-32)/9)*5
 
 def celsius(f):
     return ((f-32)/9)*5
