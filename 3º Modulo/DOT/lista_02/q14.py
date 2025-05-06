b2e7f40f12bad1badccd7b3aed9188e0f35c1b52
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 16:44:52 -0300

    q14 list2

--- 3º Modulo/DOT/lista_02/q14.py
+++ 3º Modulo/DOT/lista_02/q14.py
@@ -1,13 +1,30 @@
 # 14) Ler uma lista C de 10 elementos inteiros, trocar todos os valores negativos da lista C por 0. Escrever a lista C modificada.
 
+import unittest
+
 def modificar_lista(lista):
-    for i in range(len(lista)):
-        if lista [i] < 0:
-            lista [i] = 0
-    return lista
+    return [x if x >= 0 else 0 for x in lista]
 
-C = [int(input(f'Elemento {i + 1}: ')) for i in range(10)]
+def ler_lista(tamanho):
+    return [int(input(f'Elemento {i + 1}: ')) for i in range(tamanho)]
 
-C_modificada = modificar_lista(C)
+def main():
+    tamanho = 10
+    C = ler_lista(tamanho)
+    C_modificada = modificar_lista(C)
+    print("Lista C modificada:", C_modificada)
 
-print("Lista C modificada", C_modificada)
+class TestModificarLista(unittest.TestCase):
+    def test_modificar_lista(self):
+        lista = [-1, 2, -3, 4, -5]
+        lista_modificada = modificar_lista(lista)
+        self.assertEqual(lista_modificada, [0, 2, 0, 4, 0])
+
+    def test_modificar_lista_sem_negativos(self):
+        lista = [1, 2, 3, 4, 5]
+        lista_modificada = modificar_lista(lista)
+        self.assertEqual(lista_modificada, [1, 2, 3, 4, 5])
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+    main()
