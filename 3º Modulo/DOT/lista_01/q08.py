fa77e990bed000298b9468aa5edc6717ded7f45f
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 14:47:12 -0300

    q08 list1

--- 3º Modulo/DOT/lista_01/q08.py
+++ 3º Modulo/DOT/lista_01/q08.py
@@ -1,8 +1,10 @@
-#8. Escreva uma função que lê um caractere digitado pelo usuário e retorna este caractere somente se ele for igual a 'S' ou 'N'. Se o caractere não for nem 'S' nem 'N', a função imprime a mensagem 'Caractere inválido. Digite novamente'. Use esta função em um  programa que fica lendo do usuário um número qualquer e imprime este número ao cubo na tela. O programa deve ficar lendo os  números até o usuário responder 'N' à pergunta se ele deseja continuar ou não. 
+# 8. Escreva uma função que lê um caractere digitado pelo usuário e retorna este caractere somente se ele for igual a 'S' ou 'N'. Se o caractere não for nem 'S' nem 'N', a função imprime a mensagem 'Caractere inválido. Digite novamente'. Use esta função em um  programa que fica lendo do usuário um número qualquer e imprime este número ao cubo na tela. O programa deve ficar lendo os  números até o usuário responder 'N' à pergunta se ele deseja continuar ou não. 
 
-def ler_carac():
+import unittest
+
+def ler_carac(input_func=input):
     while True:
-        caracter = input('Deseja continuar? (S/N): ')
+        caracter = input_func('Deseja continuar? (S/N): ').strip().upper()
         if caracter == 'S':
             return 'S'
         elif caracter == 'N':
@@ -10,12 +12,35 @@ def ler_carac():
         else:
             print('\nCaractere inválido. Digite novamente.')
 
-while True:
+def ao_cubo(n):
+    return n ** 3
+
+if __name__ == '__main__':
     try:
-        n = float(input('Digite um número: '))
-        print(f'{n} ao cubo é {n ** 3}')
-        resposta = ler_carac()
-        if resposta == 'N':
-            break
+        while True:
+            numero = float(input('Digite um número: '))
+            print(f'{numero} ao cubo é {ao_cubo(numero)}')
+            if ler_carac() == 'N':
+                break
     except ValueError:
         print('\nAlgo de errado não está certo, tente novamente.')
+
+class TestCuboEEntrada(unittest.TestCase):
+    def test_ao_cubo_positivo(self):
+        self.assertEqual(ao_cubo(3), 27)
+
+    def test_ao_cubo_zero(self):
+        self.assertEqual(ao_cubo(0), 0)
+
+    def test_ao_cubo_negativo(self):
+        self.assertEqual(ao_cubo(-2), -8)
+
+    def test_ler_carac_s(self):
+        self.assertEqual(ler_carac(lambda _: 'S'), 'S')
+
+    def test_ler_carac_n(self):
+        self.assertEqual(ler_carac(lambda _: 'N'), 'N')
+
+    def test_ler_carac_invalido_then_s(self):
+        inputs = iter(['x', 'S'])  # primeiro inválido, depois válido
+        self.assertEqual(ler_carac(lambda _: next(inputs)), 'S')
