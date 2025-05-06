10a120ef696f44c51d35bd525f220796baab2639
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 15:46:51 -0300

    q09 list2

--- 3º Modulo/DOT/lista_02/q09.py
+++ 3º Modulo/DOT/lista_02/q09.py
@@ -1,6 +1,26 @@
 # 9) Dada uma lista X numérica contendo 5 elementos, fazer um programa que crie e exiba na tela uma lista Y. A lista Y deverá conter o mesmo conteúdo da lista X na ordem inversa.
 
+import unittest
+
 def inverter_lista(lista):
-    lista_invertida = lista[::-1]
-    print("Lista x:", lista)
-    print("Lista y (invertida):", lista_invertida)
+    return lista[::-1]
+
+def main():
+    lista_x = [1, 2, 3, 4, 5]
+    lista_y = inverter_lista(lista_x)
+    print("Lista x:", lista_x)
+    print("Lista y (invertida):", lista_y)
+
+class TestInverterLista(unittest.TestCase):
+    def test_inverter_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        lista_invertida = inverter_lista(lista)
+        self.assertEqual(lista_invertida, [5, 4, 3, 2, 1])
+
+    def test_inverter_lista_vazia(self):
+        lista = []
+        lista_invertida = inverter_lista(lista)
+        self.assertEqual(lista_invertida, [])
+
+unittest.main(exit=False)
+main()
