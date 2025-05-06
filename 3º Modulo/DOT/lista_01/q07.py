269e4b12fc76d1bafd329777c9f87e2c3585e851
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 14:45:06 -0300

    q07 list1

--- 3º Modulo/DOT/lista_01/q07.py
+++ 3º Modulo/DOT/lista_01/q07.py
@@ -1,15 +1,32 @@
-#7. Faça um programa para calcular o Fatorial de um número. Para o cálculo do fatorial, sabemos que N! depende de (N-1)!; este por sua vez depende de (N-2)!; e, assim por diante, até que N seja 1, quando então tem-se que fatorial de 1 é igual a 1 mesmo. Utilize uma função que recebe como parâmetro de entrada o número a ser calculado o fatorial, do tipo inteiro, e retorna o fatorial deste número, também do tipo inteiro.
+# 7. Faça um programa para calcular o Fatorial de um número. Para o cálculo do fatorial, sabemos que N! depende de (N-1)!; este por sua vez depende de (N-2)!; e, assim por diante, até que N seja 1, quando então tem-se que fatorial de 1 é igual a 1 mesmo. Utilize uma função que recebe como parâmetro de entrada o número a ser calculado o fatorial, do tipo inteiro, e retorna o fatorial deste número, também do tipo inteiro.
+
+import unittest
 
 def fatorial(f):
     fat = 1
     for i in range(1, f + 1):
-        fat = fat * i
+        fat *= i
     return fat
 
-while True:
+if __name__ == '__main__':
     try:
-        num = int (input("\nDigite um número:"))
-        print("\nO fatorial de %d é: %d" %(num, fatorial(num)))
-        break
-    except:
+        num = int(input("\nDigite um número: "))
+        if num < 0:
+            print("\nNão existe fatorial de número negativo.")
+        else:
+            print(f"\nO fatorial de {num} é: {fatorial(num)}")
+    except ValueError:
         print("\nAlgo de errado não está certo, tente novamente.")
+
+class TestFatorial(unittest.TestCase):
+    def test_fatorial_0(self):
+        self.assertEqual(fatorial(0), 1)
+
+    def test_fatorial_1(self):
+        self.assertEqual(fatorial(1), 1)
+
+    def test_fatorial_5(self):
+        self.assertEqual(fatorial(5), 120)
+
+    def test_fatorial_7(self):
+        self.assertEqual(fatorial(7), 5040)
