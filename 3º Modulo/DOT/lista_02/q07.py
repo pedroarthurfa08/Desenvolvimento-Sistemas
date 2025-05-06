03cd39e670f44994c2e0ebb29c46fb0a81b817e6
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 15:42:32 -0300

    q07 list2

--- 3º Modulo/DOT/lista_02/q07.py
+++ 3º Modulo/DOT/lista_02/q07.py
@@ -1,13 +1,28 @@
 # 7) Dada uma lista contendo 10 elementos numéricos, elabore um programa que verifique se um outro valor dado pertence ou não à lista.
 
+import unittest
+
 def verificar_valor_na_lista(lista, valor):
-    if valor in lista:
+    return valor in lista
+
+def main():
+    lista = [10, 25, 30, 45, 50, 60, 70, 85, 90, 100]
+    valor = float(input("Digite um valor para verificar se ele pertence à lista: "))
+    if verificar_valor_na_lista(lista, valor):
         print(f"O valor {valor} está na lista.")
     else:
         print(f"O valor {valor} não está na lista.")
 
-lista = [10, 25, 30, 45, 50, 60, 70, 85, 90, 100]
+class TestVerificarValorNaLista(unittest.TestCase):
+    def test_valor_esta_na_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        valor = 3
+        self.assertTrue(verificar_valor_na_lista(lista, valor))
 
-valor = float(input("Digite um valor para verificar se ele pertence à lista: "))
+    def test_valor_nao_esta_na_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        valor = 6
+        self.assertFalse(verificar_valor_na_lista(lista, valor))
 
-verificar_valor_na_lista(lista, valor)
+unittest.main(exit=False)
+main()
