ad457132aac6bce3701b2c6525839b86d2d9c1f2
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 15:16:49 -0300

    q14 list1

--- 3º Modulo/DOT/lista_01/q14.py
+++ 3º Modulo/DOT/lista_01/q14.py
@@ -1,18 +1,31 @@
-#14. Escreva uma função que recebe por parâmetro um valor inteiro e positivo N e retorna o valor de S. S = 1 + 1/1! + ½! + 1/3! + 1 /N!
+# 14. Escreva uma função que recebe por parâmetro um valor inteiro e positivo N e retorna o valor de S. S = 1 + 1/1! + ½! + 1/3! + 1 /N!
 
+import unittest
 import math
 
 def calculo_S(N):
     if N <= 0:
-        raise ValueError("\nN dever ser um valor inteiro positivo.")
-    
-    S = sum(1 / math.factorial(i) for i in range(1, N + 1))
-    return S
+        raise ValueError("N deve ser um valor inteiro positivo.")
+    return 1 + sum(1 / math.factorial(i) for i in range(1, N + 1))
 
-while True:
-    try:
-        N = int(input("\n Digite um número interio positivo: "))
-        print("O valor de S é", calculo_S(N))
-        break
-    except ValueError:
-        print("\nAlgo de errado não está certo, tente novamente.")
+class TestCalculoS(unittest.TestCase):
+    def test_calculo_S_positivo(self):
+        self.assertAlmostEqual(calculo_S(4), 2.708333333333333)
+
+    def test_calculo_S_um(self):
+        self.assertAlmostEqual(calculo_S(1), 2)
+
+    def test_calculo_S_invalido(self):
+        with self.assertRaises(ValueError):
+            calculo_S(-1)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
+    while True:
+        try:
+            N = int(input("\nDigite um número inteiro positivo: "))
+            print(f"O valor de S é {calculo_S(N):.4f}")
+            break
+        except ValueError as e:
+            print(f"\n{e}")
