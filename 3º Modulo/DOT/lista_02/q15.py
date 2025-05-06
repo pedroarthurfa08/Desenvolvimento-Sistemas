de344e511bc37a27a21e68224e01b5549e22cbd7
Pedro Arthur <pedroarthurfa08@gmail.com>
2025-05-06 16:47:21 -0300

    q15 list2

--- 3º Modulo/DOT/lista_02/q15.py
+++ 3º Modulo/DOT/lista_02/q15.py
@@ -1,11 +1,31 @@
 # 15) Ler uma lista D de 10 elementos. Criar uma lista E, com todos os elementos de D na ordem inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será o segundo e assimpor diante. Escrever todo a lista D e todo a lista E.
 
-def invert_list(list):
-    return list[::-1]
+import unittest
 
-D = [int(input(f'Elemento {i + 1}: ')) for i in range(10)]
+def inverter_lista(lista):
+    return lista[::-1]
 
-E = invert_list (D)
+def ler_lista(tamanho):
+    return [int(input(f'Elemento {i + 1}: ')) for i in range(tamanho)]
 
-print('Lista D', D)
-print('Lista E (inversa)', E)
+def main():
+    tamanho = 10
+    D = ler_lista(tamanho)
+    E = inverter_lista(D)
+    print('Lista D:', D)
+    print('Lista E (inversa):', E)
+
+class TestInverterLista(unittest.TestCase):
+    def test_inverter_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        lista_invertida = inverter_lista(lista)
+        self.assertEqual(lista_invertida, [5, 4, 3, 2, 1])
+
+    def test_inverter_lista_vazia(self):
+        lista = []
+        lista_invertida = inverter_lista(lista)
+        self.assertEqual(lista_invertida, [])
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+    main()
