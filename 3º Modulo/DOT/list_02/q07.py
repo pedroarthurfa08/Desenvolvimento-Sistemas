ba00f6c2042ba93ee0a99726395eae0b83a168f7
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 16:02:48 -0300

    q07

--- 3º Modulo/DOT/list_02/q07.py
+++ 3º Modulo/DOT/list_02/q07.py
@@ -24,5 +24,33 @@ class TestVerificarValorNaLista(unittest.TestCase):
         valor = 6
         self.assertFalse(verificar_valor_na_lista(lista, valor))
 
+if __name__ == "__main__":
+    unittest.main(exit=False)
+    main()
+    
+import unittest
+'''
+def verificar_valor_na_lista(lista, valor):
+    return valor in lista
+
+def main():
+    lista = [10, 25, 30, 45, 50, 60, 70, 85, 90, 100]
+    valor = float(input("Digite um valor para verificar se ele pertence à lista: "))
+    if verificar_valor_na_lista(lista, valor):
+        print(f"O valor {valor} está na lista.")
+    else:
+        print(f"O valor {valor} não está na lista.")
+
+class TestVerificarValorNaLista(unittest.TestCase):
+    def test_valor_esta_na_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        valor = 3
+        self.assertTrue(verificar_valor_na_lista(lista, valor))
+
+    def test_valor_nao_esta_na_lista(self):
+        lista = [1, 2, 3, 4, 5]
+        valor = 6
+        self.assertFalse(verificar_valor_na_lista(lista, valor))
+
 unittest.main(exit=False)
-main()
+main()'''
