eb35a41c93a5621f69f4f10668275803056ec2e7
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 15:16:11 -0300

    q02

--- 3º Modulo/DOT/lista_01/q02.py
+++ 3º Modulo/DOT/lista_01/q02.py
@@ -1,6 +1,6 @@
 # 2. Escreva um programa que leia o raio de um círculo e faça duas funções: uma função chamada área que calcula e retorna a área do círculo e outra função chamada perímetro que calcula e retorna o perímetro do círculo. Área = PI * r2; Perímetro = PI * 2 * r;
 
-import unittest
+"""import unittest
 import math
 
 def area(r):
@@ -33,4 +33,27 @@ if __name__ == '__main__':
                 print(f"O perímetro do círculo é: {perimetro(raio):.2f}")
                 break
         except ValueError:
-            print("\nEntrada inválida. Digite um número válido.")
+            print("\nEntrada inválida. Digite um número válido.")"""
+
+import unittest
+import math
+
+def area(r):
+    return math.pi * r ** 2
+
+def perimetro(r):
+    return math.pi * 2 * r
+
+class TestCirculo(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(area(1), math.pi)
+        self.assertAlmostEqual(area(0), 0)
+        self.assertAlmostEqual(area(2), 4 * math.pi)
+
+    def test_perimetro(self):
+        self.assertAlmostEqual(perimetro(1), 2 * math.pi)
+        self.assertAlmostEqual(perimetro(0), 0)
+        self.assertAlmostEqual(perimetro(2), 4 * math.pi)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
