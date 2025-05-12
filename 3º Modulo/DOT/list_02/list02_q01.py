0c332322fe353cf0f1ca6ea5d4ff68ab14be0f57
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 15:53:43 -0300

    q01

--- 3º Modulo/DOT/list_02/list02_q01.py
+++ 3º Modulo/DOT/list_02/list02_q01.py
@@ -36,6 +36,39 @@ class TestSepararParesImpares(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(exit=False)
+    
+'''import unittest
+import random
+
+def gerar_numeros(tamanho):
+    return [random.randint(0, 1000) for _ in range(tamanho)]
+
+def separar_pares_impares(numeros):
+    pares = [n for n in numeros if n % 2 == 0]
+    impares = [n for n in numeros if n % 2 != 0]
+    return pares, impares
+
+class TestSepararParesImpares(unittest.TestCase):
+    def test_separar_pares_impares(self):
+        numeros = [1, 2, 3, 4, 5]
+        pares, impares = separar_pares_impares(numeros)
+        self.assertEqual(pares, [2, 4])
+        self.assertEqual(impares, [1, 3, 5])
+
+    def test_separar_pares_impares_somente_pares(self):
+        numeros = [2, 4, 6, 8, 10]
+        pares, impares = separar_pares_impares(numeros)
+        self.assertEqual(pares, [2, 4, 6, 8, 10])
+        self.assertEqual(impares, [])
+
+    def test_separar_pares_impares_somente_impares(self):
+        numeros = [1, 3, 5, 7, 9]
+        pares, impares = separar_pares_impares(numeros)
+        self.assertEqual(pares, [])
+        self.assertEqual(impares, [1, 3, 5, 7, 9])
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
 
     numeros = gerar_numeros(100)
     pares, impares = separar_pares_impares(numeros)
@@ -44,4 +77,4 @@ if __name__ == "__main__":
     print(f"Os números pares são: {pares}")
     print("--------------------------------------------------")
     print(f"Quantidade de números ímpares: {len(impares)}")
-    print(f"Os números ímpares são: {impares}")
+    print(f"Os números ímpares são: {impares}")'''
