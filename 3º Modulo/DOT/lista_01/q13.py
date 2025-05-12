cdc82af1a8a46fd842df0cbc6b9091c47f456522
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 15:33:13 -0300

    q13

--- 3º Modulo/DOT/lista_01/q13.py
+++ 3º Modulo/DOT/lista_01/q13.py
@@ -21,10 +21,31 @@ class TestCalculoS(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main(exit=False)
 
+'''import unittest
+
+def calculo_S(N):
+    if N <= 0:
+        raise ValueError("N deve ser um valor inteiro positivo.")
+    return sum(1 / i for i in range(1, N + 1))
+
+class TestCalculoS(unittest.TestCase):
+    def test_calculo_S_positivo(self):
+        self.assertAlmostEqual(calculo_S(4), 2.083333333333333)
+
+    def test_calculo_S_um(self):
+        self.assertEqual(calculo_S(1), 1)
+
+    def test_calculo_S_invalido(self):
+        with self.assertRaises(ValueError):
+            calculo_S(-1)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
     while True:
         try:
             N = int(input("\nDigite um número inteiro positivo: "))
             print(f"O valor de S é {calculo_S(N):.4f}")
             break
         except ValueError as e:
-            print(f"\n{e}")
+            print(f"\n{e}")'''
