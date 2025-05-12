5763d47d6d52c023592e893fce56d69615b9a90c
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 15:20:26 -0300

    q05

--- 3º Modulo/DOT/lista_01/q05.py
+++ 3º Modulo/DOT/lista_01/q05.py
@@ -27,6 +27,29 @@ class TestPesoIdeal(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main(exit=False)
 
+'''import unittest
+
+def peso_ideal(h, sexo):
+    if sexo == 1:
+        return (62.1 * h) - 44.7
+    elif sexo == 2:
+        return (72.7 * h) - 58
+    else:
+        return None
+
+class TestPesoIdeal(unittest.TestCase):
+    def test_mulher(self):
+        self.assertAlmostEqual(peso_ideal(1.60, 1), (62.1 * 1.60) - 44.7)
+
+    def test_homem(self):
+        self.assertAlmostEqual(peso_ideal(1.75, 2), (72.7 * 1.75) - 58)
+
+    def test_sexo_invalido(self):
+        self.assertIsNone(peso_ideal(1.75, 3))
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
     while True:
         try:
             sexo = int(input("\nDigite 1 para mulher ou 2 para homem: "))
@@ -38,4 +61,4 @@ if __name__ == '__main__':
                 print(f"\nSeu peso ideal é: {peso:.2f} kg")
                 break
         except ValueError:
-            print("\nAlgo de errado não está certo, tente novamente.")
+            print("\nAlgo de errado não está certo, tente novamente.")'''
