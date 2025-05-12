870c3ec8db67581b2cf5a2761731ead4004f62a5
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-12 16:00:26 -0300

    q04

--- 3º Modulo/DOT/list_02/q05.py
+++ 3º Modulo/DOT/list_02/q05.py
@@ -33,4 +33,38 @@ class TestIntercalarListas(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(exit=False)
-    main()
+
+'''import unittest
+
+def coletar_numeros(qnt):
+    numeros = []
+    for i in range(qnt):
+        valor = int(input(f"Digite o {i + 1}º valor: "))
+        numeros.append(valor)
+    return numeros
+
+def intercalar_listas(lista1, lista2):
+    return [valor for par in zip(lista1, lista2) for valor in par]
+
+def main():
+    print("Digite os valores da primeira lista:")
+    lista1 = coletar_numeros(10)
+    print("\nDigite os valores da segunda lista:")
+    lista2 = coletar_numeros(10)
+
+    lista_intercalada = intercalar_listas(lista1, lista2)
+
+    print("\nLista 1:", lista1)
+    print("Lista 2:", lista2)
+    print("Lista Intercalada:", lista_intercalada)
+
+class TestIntercalarListas(unittest.TestCase):
+    def test_intercalar_listas(self):
+        lista1 = [1, 3, 5]
+        lista2 = [2, 4, 6]
+        lista_intercalada = intercalar_listas(lista1, lista2)
+        self.assertEqual(lista_intercalada, [1, 2, 3, 4, 5, 6])
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+    main()'''
