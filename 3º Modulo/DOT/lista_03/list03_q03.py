0ff796f7154f15aaec70a5a65a6b84a09a3512da
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-05-19 16:36:23 -0300

    q03

--- 3º Modulo/DOT/lista_03/list03_q03.py
+++ 3º Modulo/DOT/lista_03/list03_q03.py
@@ -1,1 +1,9 @@
 # 3) Escreva uma função que recebe uma lista com n números inteiros, e determina a maior soma de um segmento com 2 valores. Ex: [5, -2, -2, -7, 3, 15, 10, -3, 9, -6, 4, 1] = 25
+
+def maior_soma_segmento(numeros):
+    return max(numeros[i] + numeros[i + 1] for i in range(len(numeros) - 1))
+
+# Testes
+assert maior_soma_segmento([5, -2, -2, -7, 3, 15, 10, -3, 9, -6, 4, 1]) == 25
+assert maior_soma_segmento([1, 2]) == 3
+assert maior_soma_segmento([-1, -2]) == -3
